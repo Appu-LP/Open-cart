db528e860997a7a69e2c945df05d3619bbaaaa82
Appu L-P <jeevanpgowda27@gmail.com>
2026-09-20 20:25:58 +0530

    Latest commit

--- src/test/java/DataFromExcelFile/ExcelFile.java
+++ src/test/java/DataFromExcelFile/ExcelFile.java
@@ -14,7 +14,7 @@ public class ExcelFile {
 
 	public static void main(String[] args) throws Throwable {
 
-		File file = new File("");
+		File file = new File("File Name");
 		FileInputStream fileInput = new FileInputStream(file);
 		Workbook book = WorkbookFactory.create(fileInput);
 	
